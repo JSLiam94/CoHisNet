4de2aee812a7dc844f628e4ac7e724f22f4d62e3
蒋硕 <111181768+JSLiam94@users.noreply.github.com>
2025-01-29 14:42:08 +0800

    Update model.py
    Multi-Scale Adaptive Swin KANsformer

--- model.py
+++ model.py
@@ -12,6 +12,7 @@ import torch.nn.functional as F
 import torch.utils.checkpoint as checkpoint
 import numpy as np
 from typing import Optional
+import math
 from src.efficient_kan import KAN
 
 
@@ -757,9 +758,7 @@ class SwinKANsformerBlock(nn.Module):
         return x
 
 class SwinKANsformer(nn.Module):
-    r""" Swin Transformer
-        A PyTorch impl of : `Swin Transformer: Hierarchical Vision Transformer using Shifted Windows`  -
-          https://arxiv.org/pdf/2103.14030
+    r""" Swin KANsformer
 
     Args:
         patch_size (int | tuple(int)): Patch size. Default: 4
@@ -957,7 +956,150 @@ class SwinKANsformer_li(nn.Module):
         return x
     
 
+class Swin_Fusion_KANsformer(nn.Module):
+    r""" Swin Transformer
+        A PyTorch impl of : `Swin Transformer: Hierarchical Vision Transformer using Shifted Windows`  -
+          https://arxiv.org/pdf/2103.14030
 
+    Args:
+        patch_size (int | tuple(int)): Patch size. Default: 4
+        in_chans (int): Number of input image channels. Default: 3
+        num_classes (int): Number of classes for classification head. Default: 1000
+        embed_dim (int): Patch embedding dimension. Default: 96
+        depths (tuple(int)): Depth of each Swin Transformer layer.
+        num_heads (tuple(int)): Number of attention heads in different layers.
+        window_size (int): Window size. Default: 7
+        mlp_ratio (float): Ratio of mlp hidden dim to embedding dim. Default: 4
+        qkv_bias (bool): If True, add a learnable bias to query, key, value. Default: True
+        drop_rate (float): Dropout rate. Default: 0
+        attn_drop_rate (float): Attention dropout rate. Default: 0
+        drop_path_rate (float): Stochastic depth rate. Default: 0.1
+        norm_layer (nn.Module): Normalization layer. Default: nn.LayerNorm.
+        patch_norm (bool): If True, add normalization after patch embedding. Default: True
+        use_checkpoint (bool): Whether to use checkpointing to save memory. Default: False
+    """
+
+    def __init__(self, patch_size=4, in_chans=3, num_classes=1000,
+                 embed_dim=96, depths=(2, 2, 6, 2), num_heads=(3, 6, 12, 24),
+                 window_size=7, mlp_ratio=4., qkv_bias=True,
+                 drop_rate=0., attn_drop_rate=0., drop_path_rate=0.1,
+                 norm_layer=nn.LayerNorm, patch_norm=True,
+                 use_checkpoint=False,image_size=(224,224),device='cpu', **kwargs):
+        super().__init__()
+        self.window_size = window_size
+        self.num_classes = num_classes
+        self.num_layers = len(depths)
+        self.embed_dim = embed_dim
+        self.patch_norm = patch_norm
+        self.step_count = 0
+        # stage4输出特征矩阵的channels
+        self.num_features = int(embed_dim * 2 ** (self.num_layers - 1))
+        self.mlp_ratio = mlp_ratio
+
+        self.device=device
+
+        # split image into non-overlapping patches
+        self.patch_embed = PatchEmbed(
+            patch_size=patch_size, in_c=in_chans, embed_dim=embed_dim,
+            norm_layer=norm_layer if self.patch_norm else None)
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+        # stochastic depth
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]  # stochastic depth decay rule
+
+        # build layers
+        self.layers = nn.ModuleList()
+        for i_layer in range(self.num_layers):
+            # 注意这里构建的stage和论文图中有些差异
+            # 这里的stage不包含该stage的patch_merging层，包含的是下个stage的
+            layers = BasicLayer(dim=int(embed_dim * 2 ** i_layer),
+                                depth=depths[i_layer],
+                                num_heads=num_heads[i_layer],
+                                window_size=window_size,
+                                mlp_ratio=self.mlp_ratio,
+                                qkv_bias=qkv_bias,
+                                drop=drop_rate,
+                                attn_drop=attn_drop_rate,
+                                drop_path=dpr[sum(depths[:i_layer]):sum(depths[:i_layer + 1])],
+                                norm_layer=norm_layer,
+                                downsample=PatchMerging if (i_layer < self.num_layers - 1) else None,
+                                use_checkpoint=use_checkpoint)
+            self.layers.append(layers)
+
+        self.norm = norm_layer(self.num_features)
+        self.avgpool = nn.AdaptiveAvgPool1d(1)
+        
+        # 使用 KAN 进行多尺度特征融合，在forward中根据输入维度动态初始化
+        
+        # 前3个阶段的特征权重（可学习参数）,初始均为1
+        self.stage_weights = nn.Parameter(torch.ones(self.num_layers))
+        
+        #self.head = nn.Linear(self.num_features, num_classes) if num_classes > 0 else nn.Identity()
+        self.head = KAN([self.num_features, 8, num_classes]) if num_classes > 0 else nn.Identity()
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            nn.init.trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def forward(self, x):
+        B, H, W, C = x.shape
+        x, H, W = self.patch_embed(x)
+        x = self.pos_drop(x)
+
+        multi_scale_features = []
+
+        for i, layer in enumerate(self.layers):
+            x, H, W = layer(x, H, W)
+            if i < self.num_layers - 1:
+                last_H = H
+                last_W = W
+                feature_map = x.view(x.shape[0], H, W, -1).permute(0, 3, 1, 2)
+                feature_map = feature_map * self.stage_weights[i]
+                multi_scale_features.append(feature_map)
+
+        if len(multi_scale_features) > 0:
+            # 调整特征图大小并拼接
+            for i in range(len(multi_scale_features)):
+                multi_scale_features[i] = F.interpolate(multi_scale_features[i], size=(last_H, last_W), mode='bilinear', align_corners=False)
+            fused_features = torch.cat(multi_scale_features, dim=1)  # [B, sum_Ci, H, W]
+            
+            B, sum_Ci, H_f, W_f = fused_features.shape
+            # 调整形状以处理每个空间位置
+            fused_features = fused_features.permute(0, 2, 3, 1).reshape(-1, sum_Ci)  # [B*H_f*W_f, sum_Ci]
+            
+            # 应用KAN进行特征融合
+            if(self.step_count==0):#初始化kan
+                self.fusion_kan = KAN([sum_Ci, 8,self.num_features]).to(self.device)
+                with open('fusion_dim.txt', 'w') as f:
+                    f.write(f"'fusion_dim : '{sum_Ci}")
+                self.step_count += 0.0001
+                if(self.step_count>10000):
+                    self.step_count = 2
+            fused_features = self.fusion_kan(fused_features)  # [B*H_f*W_f, num_features]
+            
+            # 恢复形状并与原始特征相加
+            fused_features = fused_features.view(B, H_f, W_f, -1).permute(0, 3, 1, 2)  # [B, C, H, W]
+            fused_features = fused_features.flatten(2).transpose(1, 2)  # [B, L, C]
+            
+            x = x + fused_features  # 直接相加，形状一致
+
+        x = self.norm(x)
+        x = self.avgpool(x.transpose(1, 2))
+        x = torch.flatten(x, 1)
+        x = self.head(x)
+        #将self.stage_weights写入txt文件查看
+        if(self.step_count % 2000==1):
+            with open('stage_weights.txt','w') as f:
+                for i in range(self.num_layers-1):
+                    f.write(f"'stage '{i} 'weights: '{self.stage_weights[i].item()}\n")
+        return x
 def swin_tiny_patch4_window7_224(num_classes: int = 1000, **kwargs):
     # trained ImageNet-1K
     # https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth
@@ -984,6 +1126,20 @@ def swinkan_micro_patch4_window7_224(num_classes: int = 1000, **kwargs):
                             **kwargs)
     return model
 
+def swin_fusion_kan_micro_patch4_window7_224(num_classes: int = 1000, image_size=(224,224),device='cpu',**kwargs):
+    # trained ImageNet-1K
+    # https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth
+    model = Swin_Fusion_KANsformer(in_chans=3,
+                            patch_size=4,
+                            window_size=7,
+                            embed_dim=24,
+                            depths=(2, 2, 8, 2),
+                            num_heads=(4, 8, 16, 32),
+                            num_classes=num_classes,
+                            image_size=image_size,
+                            device=device,
+                            **kwargs)
+    return model
 def swinkan_li_micro_patch4_window7_224(num_classes: int = 1000, **kwargs):
     # trained ImageNet-1K
     # https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth
@@ -1008,6 +1164,19 @@ def swinkan_tiny_patch4_window7_224(num_classes: int = 1000, **kwargs):
                             num_classes=num_classes,
                             **kwargs)
     return model
+
+def swin_fusion_kan_tiny_patch4_window7_224(num_classes: int = 1000, **kwargs):
+ 
+    model = Swin_Fusion_KANsformer(in_chans=3,
+                            patch_size=4,
+                            window_size=7,
+                            embed_dim=48,
+                            depths=(2, 2, 8, 2),
+                            num_heads=(4, 8, 16, 32),
+                            num_classes=num_classes,
+                            **kwargs)
+    return model
+
 
 def swinkan_mini_patch4_window7_224(num_classes: int = 1000, **kwargs):
  
